cbbdb46ba45218ef3f8f02e8aa795804b4883808
Lora34 <my34pochta@gmail.com>
2025-03-22 22:05:37 +0300

    add db&cache values to settings

--- settings.py
+++ settings.py
@@ -2,4 +2,12 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     #GOOGLE_TOKEN_ID: str = "kjbjhtrbjvrnkregjhtb"
-    sqlite_db_name: str = 'pomodoro.db'
+    #sqlite_db_name: str = 'pomodoro.db'
+    DB_HOST: str = '0.0.0.0'
+    DB_PORT: int = 5432
+    DB_USER: str = 'postgres'
+    DB_PASSWORD: str = 'password'
+    DB_NAME: str = 'pomodoro'
+    CACHE_HOST: str = '0.0.0.0'
+    CACHE_PORT: int = 5432
+    CACHE_DB: int = 0

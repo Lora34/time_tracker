7c4b4bbac2fca7545cc53993dee8b1735a973cf4
Lora34 <my34pochta@gmail.com>
2025-03-22 22:14:31 +0300

    fix database&cache accessor

--- settings.py
+++ settings.py
@@ -7,7 +7,12 @@ class Settings(BaseSettings):
     DB_PORT: int = 5432
     DB_USER: str = 'postgres'
     DB_PASSWORD: str = 'password'
+    DB_DRIVER: str = 'sqlite:///pomodoro.db'
     DB_NAME: str = 'pomodoro'
     CACHE_HOST: str = '0.0.0.0'
     CACHE_PORT: int = 5432
     CACHE_DB: int = 0
+
+    @property
+    def db_url(self):
+        return f'{self.DB_PASSWORD}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
